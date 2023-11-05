3b18c64844d7d99a64a210426fcb78a1517d9a6f
kharle14 <enriquezkharl14@gmail.com>
2023-11-05 20:29:26 +0800

    Button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import Button from '@/components/Button';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
       <h1>main</h1>
+      <Button variant="outline">Test</Button>
     </div>
   );
 }
